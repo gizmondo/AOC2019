45d68f560a187fb9255519b34bb76033ec28d553
Alex Aktsipetrov <alex.akts@gmail.com>
2019-12-10 00:30:54 +0300

    Finish day9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -189,10 +189,18 @@ fn part1(program: &Vec<i64>) -> AocResult<i64> {
     Ok(intcode.input.recv()?)
 }
 
+fn part2(program: &Vec<i64>) -> AocResult<i64> {
+    let (sender, receiver) = channel();
+    sender.send(2)?;
+    let mut intcode = Intcode::new(program.clone(), receiver, sender);
+    intcode.exec()?;
+    Ok(intcode.input.recv()?)
+}
 
 fn main() -> AocResult<()> {
     let input = fs::read_to_string("input.txt")?;
     let program = input.split(',').map(|s| s.parse::<i64>()).collect::<Result<Vec<_>, _>>()?;
     println!("{:?}", part1(&program)?);
+    println!("{:?}", part2(&program)?);
     Ok(())
 }
